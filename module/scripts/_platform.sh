d2a3f8b3e70de5bcb008bf9580ad154849a93ccd
Matt Jensen <mattj65816@gmail.com>
2025-09-27 11:49:16 -0500

    Toil.

--- module/scripts/_platform.sh
+++ module/scripts/_platform.sh
@@ -14,3 +14,16 @@ function is_platform_mac() {
 function is_platform_linux() {
     [[ "$OSTYPE" == "linux-gnu"* ]]
 }
+
+function cpu_count() {
+    if is_platform_mac; then
+        sysctl -n hw.ncpu
+    elif is_platform_linux; then
+        nproc
+    elif is_platform_windows; then
+        # On Windows, use the NUMBER_OF_PROCESSORS environment variable
+        echo "$NUMBER_OF_PROCESSORS"
+    else
+        echo 1
+    fi
+}
